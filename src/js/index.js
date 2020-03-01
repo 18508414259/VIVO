7b42a6bf9bee43e414a6b5db3112b740c3d27028
longzhaoxing <61545123+18508414259@users.noreply.github.com>
2020-03-01 20:09:53 +0800

    首页

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+ 
+window.onload=function(){
+    $(".listUl").mouseover(function(){
+       
+            $(this)
+            // .stop()
+            // .finish()
+            .next()
+            .finish()
+            .slideDown(500)
+            // .finish()
+    })
+    
+    $(".listUl").mouseleave(function(){
+        $(this)
+        .stop(1000)
+        .next()
+        .finish()
+        .slideUp(500)
+
+    })
+ 
+
+}
+   
