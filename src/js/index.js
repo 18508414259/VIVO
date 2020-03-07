0d978d1503df0bc84fecaae2918e94b0bd6fcdbe
longzhaoxing <61545123+18508414259@users.noreply.github.com>
2020-03-07 09:57:54 +0800

    完善

--- src/js/index.js
+++ src/js/index.js
@@ -123,3 +123,28 @@ $(function(){
         window.location.href='./list.html'
     })
 })
+
+
+$(function(){
+     function getCookie(name) {
+          var strCookie = document.cookie;
+           var arrCookie = strCookie.split("; ");
+           for (var i = 0; i < arrCookie.length; i++) {
+                  var arr = arrCookie[i].split("=");
+             if (arr[0] == name)
+                  return arr[1];
+               }
+               return "";
+              }
+        
+        
+         var islogin = getCookie("userName");
+         if(islogin!==''){
+            $(".loginbf").hide();
+            $(".loginaf").show();
+         }else{
+             $(".loginbf").show();
+              $(".loginaf").hide();
+             }
+       
+})
